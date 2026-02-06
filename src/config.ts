90c8f9ed2cf13a7621ec273f6b7457161064a7cd
Vladimir Grinenko <i@tadatuta.com>
2026-02-06 04:35:37 +0300

    no wildcard for ALLOWED_ORIGIN

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ import process from 'node:process';
 
 export const config = {
   STORAGE_DIR: process.env.STORAGE_DIR || '/tmp/',
-  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || '*',
+  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || '',
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
 };
